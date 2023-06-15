73a44a2dc3fd41f6e517a296a7c71316de28d3a9
Ngoc Chung Tran <tranchung163@gmail.com>
2023-06-15 13:58:07 -0500

    update
    Update

---
+++ ThrowingBeanBag/Assets/Scripts/BallCollision.cs
@@ -0,0 +1,46 @@
+//using UnityEngine;
+
+//public class BallCollision : MonoBehaviour
+//{
+//    public float moveSpeed;
+//    private Rigidbody ballRigidbody;
+//    private Vector3 originalPosition;
+//    private bool isReturning = false;
+//    private Vector3 firstPosition = new Vector3(-1.36134f, 1.115441f, 0.2953635f);
+   
+
+//    private void Start()
+//    {
+//        ballRigidbody = GetComponent<Rigidbody>();
+//        originalPosition = firstPosition;
+//    }
+
+//    private void Update()
+//    {
+//        if (isReturning)
+//        {
+//            MoveToStartPosition();
+            
+//        }
+
+
+//    }
+
+//    private void OnCollisionEnter(Collision collision)
+//    {
+//        isReturning = true;
+//    }
+
+//    private void MoveToStartPosition()
+//    {
+//        float step = moveSpeed * Time.deltaTime;
+//        transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("mixamorig:RightHandIndex1").transform.position, step);
+
+//        if (transform.position == GameObject.Find("mixamorig:RightHandIndex1").transform.position)
+//        {
+//            isReturning = false;
+//            ballRigidbody.velocity = Vector3.zero;
+//            ballRigidbody.angularVelocity = Vector3.zero;
+//        }
+//    }
+//}
---
+++ ThrowingBeanBag/Assets/Scripts/ThrowBall.cs
@@ -0,0 +1,56 @@
+
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour
+{
+    //BallScript throwTheBall = new BallScript();
+    //// Update is called once per frame
+    //public void ThrowingBall()
+    //{
+    //    Debug.Log("Throwball");
+    //    throwTheBall.ReleaseBall();
+    //}
+    public GameObject ballPrefab;
+    public Transform throwPosition;
+    public float throwForce = 10f;
+    private Transform rightHand;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rightHand = GameObject.Find("mixamorig:RightHand").transform;
+       
+        ballPrefab.GetComponent<Transform>().parent = rightHand.parent;
+        ballPrefab.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    void ReleaseBall()
+    {
+        Debug.Log("Release the ball");
+        //GameObject ball = Instantiate(ballPrefab, throwPosition.position, Quaternion.identity);
+        //Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+        ballPrefab.GetComponent<Transform>().parent = null;
+        rb = ballPrefab.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.velocity = (ballPrefab.transform.forward * -throwForce);
+
+
+        //ballRigidbody.AddForce(throwPosition.forward * throwForce, ForceMode.Impulse);
+        //ballPrefab.GetComponent<Rigidbody>().AddForce(ballPrefab.GetComponent<Transform>().forward * throwForce);
+        //GameObject ball = Instantiate(ballPrefab, throwPosition.position, Quaternion.identity);
+        //Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+        //ballRigidbody.AddForce(throwPosition.forward * throwForce, ForceMode.Impulse);
+        //GameObject ball = Instantiate(ballPrefab, throwPosition.position, throwPosition.rotation);
+        //Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+        //ballRigidbody.AddForce(throwPosition.forward * throwForce, ForceMode.Impulse);
+    }
+
+    //void catchBall()
+    //{
+    //    Debug.Log("Catch the ball");
+    //    rightHand = GameObject.Find("mixamorig:RightHandIndex1").transform;
+    //    ballPrefab.GetComponent<Rigidbody>().useGravity = false;
+    //    ballPrefab.GetComponent<Transform>().parent = rightHand.parent;
+        
+    //}
+}
